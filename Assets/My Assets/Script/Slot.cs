05bb355e251268ffbdaa17015fc12090499cc170
milrow321 <59471824+milrow321@users.noreply.github.com>
2020-09-17 15:02:12 +0900

    slot class add

---
+++ Assets/My Assets/Script/Slot.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    public Item item;
+    public int itemCount;
+    public Image itemImage;
+
+    [SerializeField]
+    private Text text_Count;
+
+    private void SetColor(float _alpha)
+    {
+        Color color = itemImage.color;
+        color.a = _alpha;
+        itemImage.color = color;
+    }
+
+    public void AddItem(Item _item,int _count =1)
+    {
+        item = _item;
+        itemCount = _count;
+        itemImage.sprite = item.itemImage;
+
+        if (item.itemType != Item.ItemType.COOKER)
+        {
+            text_Count.text = itemCount.ToString();
+        }
+        else
+        {
+            text_Count.text = 0;
+        }
+        SetColor(1);
+    }
+
+    public void SetSlotCount(int _count)
+    {
+        itemCount += _count;
+        text_Count.text = itemCount.ToString();
+
+        if (itemCount <= 0)
+        {
+            ClearSlot();
+        }
+    }
+
+    private void ClearSlot()
+    {
+        item = null;
+        itemCount = 0;
+        itemImage.sprite = null;
+        SetColor(0);
+
+        text_Count.text = "0";
+    }
+
+}
